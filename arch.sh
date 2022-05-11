b191e966c36abf3d38233dd45b32bc43af79424d
magu <95685391+itsmagu@users.noreply.github.com>
2022-05-11 03:33:28 +0200

    Init

---
+++ arch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -a
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
+CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
+set +a
+
+echo -ne "
+-------------------------------------------------------------------------
+                          ARCH-INSTALLER           
+-------------------------------------------------------------------------
+"
+    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+      source $CONFIGS_DIR/setup.conf
+    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
+    if [[ ! $DESKTOP_ENV == server ]]; then
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
+    fi
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    cp *.log /mnt/home/$USERNAME
+
+echo -ne "
+-------------------------------------------------------------------------
+                          EJECT AND REBOOT
+-------------------------------------------------------------------------
+"
