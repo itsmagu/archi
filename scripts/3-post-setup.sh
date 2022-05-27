5d436283fb8eeb594eeabba7313898fbcd437731
magu <95685391+itsmagu@users.noreply.github.com>
2022-05-27 15:22:59 +0200

    12

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -141,7 +141,7 @@ PLYMOUTH_THEMES_DIR="$HOME/archi/configs/usr/share/plymouth/themes"
 PLYMOUTH_THEME= shuf -n1 -e connect cuts_alt loader_alt pixels sphere
 mkdir -p /usr/share/plymouth/themes
 echo 'Installing Plymouth theme...'
-cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
+cp -rvf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
 if  [[ "${FS}" == "luks" ]]; then
   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
   sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
